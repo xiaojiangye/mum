c0a41492f0cb105932e76b60a1fdc477b11ea85d
hsy <2721423445@qq.com>
2017-10-26 11:32:13 +0800

    17/10/26/11:32

--- application/index/controller/Addgoods.php
+++ application/index/controller/Addgoods.php
@@ -35,7 +35,6 @@ class Addgoods extends Controller
 		}
 		$number = $res['big_id'] .'_' . $res['id'] . '_' . time();
 
-
 		$info['number'] = $number;
 		$info['name'] = Session::get('name');
 		$info['picture'] = Session::get('picture');
@@ -44,7 +43,6 @@ class Addgoods extends Controller
 		$info['stock'] = Session::get('stock');
 
 		$res = $this->goods->add($info);
-		dump($res);
 		if(!$res)
 		{
 			$this->error('执行错误' , 'Addgoods/addGoods');
@@ -53,33 +51,6 @@ class Addgoods extends Controller
 		}
 		$this->success('执行成功!' ,  'Addgoods/addGoods');
 		
-	}
-
-	
-	/*测试 得到测试页面提交过来的数据 对图片进行上传*/
-	public function uploadPicture()
-	{	
-		//$file = request()->file('picture');
-		
-
-		// 移动到框架应用根目录/public/uploads/ 目录下
-		/*$info = $file->move(ROOT_PATH . 'public' . DS . 'uploads/goods');
-		if($info)
-		{
-			$post['picture'] = $info->getSaveName().'<br />';
-		}
-		else{
-			// 上传失败获取错误信息
-			echo $file->getError();
-		}
-*/
-		/*$res = $this->addInfo($post);
-		if(!$res)
-		{
-			$this->error('执行错误' , 'Addgoods/addGoods');
-			die;
-		}
-		$this->success('执行成功!' ,  'Addgoods/addGoods');*/
 	}
 }
 
